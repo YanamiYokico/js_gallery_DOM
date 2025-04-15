d9be2bc2b390977d5c7b0201d6af75c5749049ed
YanamiYokico <v.a.nikolojchyk@gmail.com>
2025-04-15 15:47:37 +0300

    make build

---
+++ index.3705704a.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.addEventListener("click",function(t){t.preventDefault();var a=t.target.closest("a"),n=a.href,r=a.title;e.src=n,e.alt=r});
+//# sourceMappingURL=index.3705704a.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const largeImg = document.getElementById("largeImg");
+document.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const link = e.target.closest("a");
+    const newSrc = link.href;
+    const newAlt = link.title;
+    largeImg.src = newSrc;
+    largeImg.alt = newAlt;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
