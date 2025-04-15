ae0cbb2e17280e82eb44d6a6200a4749db397227
YanamiYokico <v.a.nikolojchyk@gmail.com>
2025-04-15 15:50:49 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+
+document.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('a');
+
+  const newSrc = link.href;
+  const newAlt = link.title;
+
+  largeImg.src = newSrc;
+  largeImg.alt = newAlt;
+});
